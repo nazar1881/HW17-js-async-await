b74897f3390180d74e60b53d271612027334d429
nazar1881 <90273320+nazar1881@users.noreply.github.com>
2022-01-05 16:19:12 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,59 @@
+/////////////////First task/////////////////////////
+
+const delay = (data, ms) => {
+    return new Promise(resolve => {
+        setTimeout(() => resolve(data), ms)
+    })
+}
+
+delay({
+    name: "user"
+}, 1000).then((data) => console.log("Hello!", data))
+
+/////////////////Second task///////////////////////
+
+async function getUserInfo() {
+    return delay({
+        name: 'Vic',
+        age: 21,
+        id: 1
+    }, 1000);
+};
+
+async function getUserAvatar(info) {
+    return delay(
+        (info.avatar =
+            "https://previews.123rf.com/images/stockgiu/stockgiu1708/stockgiu170802061/83728179-avatar-sketch-of-a-funny-man-s-haed-with-sunglasses-and-hairstyle-design.jpg"),
+        1000
+    );
+}
+
+async function getUserAdditionalInfo(userInfo) {
+    return delay((userInfo.interests = ['sport', 'books']), 1000);
+}
+
+
+async function getResult() {
+    let userInfo = await getUserInfo();
+    let userAvatar = await getUserAvatar(userInfo);
+    let userAdditionalInfo = await getUserAdditionalInfo(userInfo);
+
+    console.log(userInfo);
+}
+getResult();
+
+/////////////////Third task//////////////////////////
+
+async function getUser() {
+    return {
+        name: "Vic",
+        age: 21,
+        id: 1
+    };
+}
+async function getInfo() {
+    let user = await getUser();
+    throw new Error("error");
+}
+
+getInfo().catch((error) => console.error(error));
